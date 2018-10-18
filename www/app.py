31dcb33d4ddffeb4cf43cc295047c32bee1b3a91
Li Dong <a18758@hotmail.com>
2018-10-19 10:55:19 +1100

    Webapp framework

--- www/app.py
+++ www/app.py
@@ -17,7 +17,6 @@ def init(loop):
     logging.info('Serverstarted at http://127.0.0.1:9000...')
     return srv
 
-
 loop=asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
 loop.run_forever()
