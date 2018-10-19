5b469e074d9afc7685408c1f091e41517941cd83
Li Dong <a18758@hotmail.com>
2018-10-19 16:37:08 +1100

    ORM 2nd

--- www/orm.py
+++ www/orm.py
@@ -46,3 +46,46 @@ def execute(sql, args):
             raise
         return affected
 
+class Models(dict, mataclass=ModelMetaclass):
+    def __init__(selfself, **kw):
+        super(Model, self).__init__(**kw)
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            rasie AttributeError(r"'Model' object has no attribute '%s'"% key)
+
+    def __setattr__(self,key,value):
+        self[key] = value
+
+    def getValue(self,key):
+        return getattr(self, key, None)
+
+    def getValueOrDefault(self,key):
+        value = getattr(self,key, None)
+        if value is None:
+            field =self.__mapping__[key]
+            if field.defaul is not None:
+                value = field.default() if callable(field.default) else field.default
+                logging.debug('using default value for %s: %s'% (key, str(value)))
+                setattr(self,key,value)
+        return value
+
+class Field(object):
+    def __init__(self,name, column_type, primary_key, default):
+        self.name = name
+        self.column_type = column_type
+        self.primary_key = primary_key
+        self.default = default
+
+        def __str__(self):
+            return'<%s,%s:%s>' % (self.__class__.__name__,self.column_type, self.name)
+
+
+class StringFields(Fields):
+    def __init__(self,name=None, primary_key=False,default=None, ddl='varchar(100)'):
+        super().__init__(name,ddl, primary_key,default)
+
+def ModelMetaclass(type):
+    def __new__(cls, name, bases, attrs):
