cb9856d596d61387213353f567d4119322bcc870
Dong Li <A18758@hotmail.com>
2018-10-23 22:38:50 +1100

    beta 1

---
+++ www/config_default.py
@@ -0,0 +1,22 @@
+
+# -*- coding: utf-8 -*-
+
+'''
+Default configurations.
+'''
+
+__author__ = 'Dong Li'
+
+configs = {
+    'debug': True,
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'root',
+        'password': 'password',
+        'db': 'awesome'
+    },
+    'session': {
+        'secret': 'Awesome'
+    }
+}
---
+++ www/config_override.py
@@ -0,0 +1,14 @@
+
+# -*- coding: utf-8 -*-
+
+'''
+Override configurations.
+'''
+
+__author__ = 'Dong Li'
+
+configs = {
+    'db': {
+        'host': '127.0.0.1'
+    }
+}
