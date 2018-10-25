2f4cc1f5e18178f64dce13d93b1aeafe9c113d19
Dong Li <A18758@hotmail.com>
2018-10-25 22:54:24 +1100

    bug fix for app.py

--- www/app.py
+++ www/app.py
@@ -146,7 +146,7 @@ def response_factory(app, handler):
         if isinstance(r, int) and r >= 100 and r < 600:
             return web.Response(r)
 
-                r['__user__'] = request.__user__
+                ['__user__'] = request.__user__
                 resp = web.Response(body=app['__templating__'].get_template(template).render(**r).encode('utf-8'))
                 resp.content_type = 'text/html;charset=utf-8'
                 return resp
